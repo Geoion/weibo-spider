7f1969ef07ec757d95bd1503a12f773e4dc1fc3c
siax <xin036@gmail.com>
2017-03-13 02:18:19 +0800

    连接到mongoDB

--- wb.py
+++ wb.py
@@ -1,16 +1,23 @@
 import requests
 import json
 import time
-import pymongo
+from pymongo import MongoClient
 
 URL = 'http://m.weibo.cn/container/getIndex'
-COOKIE = '_T_WM=ca68525b08d761cdd867311856eb8265; SUB=_2A251vs_2DeRxGeBP61cY9i_OzziIHXVXQNG-rDV6PUJbkdBeLWn3kW0XN-bfAMTjYLdlkD7_Bz81YkdWyQ..; SUHB=0YhhA-OgV30Ig3; SCF=As5lSygKY9mP0i5xHEK7yUN-moCtlwfnpDvtaxnBbyxos-RTOiO2FXq3Cf9q0bWF8GBHpzb1kob7rFEy_cKW-TM.; SSOLoginState=1488633767; H5_INDEX=0_all; H5_INDEX_TITLE=%E5%87%8C%E6%99%B4%E6%B7%8C%E5%A4%A9%E8%8F%B1; M_WEIBOCN_PARAMS=luicode%3D20000174%26lfid%3D1005051002861732%26fid%3D1005051002861732%26uicode%3D10000011'
+COOKIE = '_T_WM=37233bf9db6006a2f6d1d2c4305bed2b; SCF=AqK2Zs1pbHWJxHPun4b8FKuPr5mzDuIuMmEdPPNmZLAELusGwNYQlhvxXZbd6YgnvvFplSt-1nlzrL45oo2sF1w.; SUB=_2A251wEBwDeRxGeBP61cY9i_KzTyIHXVXS2A4rDV6PUJbkdBeLRfDkW2fixjv8YQuL8DQ_grfUdOhLlB63Q..; SUHB=0LcSMGyJGCLyKh; SSOLoginState=1489252384; M_WEIBOCN_PARAMS=luicode%3D20000174%26lfid%3D1005051990309453%26fid%3D1005051990309453%26uicode%3D10000011'
 HEADERS = {
         'Cookie': COOKIE,
         'Host': 'm.weibo.cn',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
     }
 UID = '5907193262'
+
+def get_db():
+    client = MongoClient('localhost', 27017)
+    db = client.weibo
+    print("连接数据库")
+    return db
+
 
 def get_userinfo(uid):
     containerid = '100505' + uid
@@ -21,8 +28,19 @@ def get_userinfo(uid):
     }
 
     userinfo = requests.get(URL,params=payload,headers=HEADERS)
-    info = json.loads(userinfo.content)
-    print(info.get("userInfo"))
+    info = json.loads(userinfo.content).get("userInfo")
+    print(info)
+
+    post = {"_id":uid,
+            "screen_name": info["screen_name"],
+            "description": info["description"],
+            "follow_count": info["follow_count"],
+            "followers_count": info["followers_count"],
+            "statuses_count": info["statuses_count"]
+    }
+
+    collection = db.userInfo
+    collection.insert(post)
 
 def get_fans(uid,page):
     containerid = '231051_-_fans_-_' +uid
@@ -59,7 +77,6 @@ def get_followers(uid,page):
         time.sleep(3)
         get_followers(UID,newpage)
 
-
 def get_tweets(uid,page):
     containerid = '107603' + uid
     payload = {
@@ -80,7 +97,33 @@ def get_tweets(uid,page):
         time.sleep(3)
         get_tweets(UID,newpage)
 
+def get_comments(tid):
+    page = 1
+    usercomments = requests.get("http://m.weibo.cn/api/comments/show?id=" + tid + "&page="+ str(page), headers= HEADERS)
+    comments = json.loads(usercomments.content)
+    print(comments.get('data'))
+
+    maxpage = comments.get('max')
+
+    if page < maxpage:
+        get_comments(tid, page+1)
+
+def get_reposttimeline(tid,page):
+    usercomments = requests.get("http://m.weibo.cn/api/statuses/repostTimeline?id=" + tid + "&page=" + str(page), headers=HEADERS)
+    comments = json.loads(usercomments.content)
+    print(comments.get('data'))
+
+    maxpage = comments.get('max')
+
+    if page < maxpage:
+        get_comments(tid, page + 1)
+
+db = get_db()
+
 get_userinfo(UID)
-get_tweets(UID,"")
-get_fans(UID,"")
-get_followers(UID,"")
+#get_tweets(UID,"")
+#get_fans(UID,"")
+#get_followers(UID,"")
+#get_reposttimeline("4084099851139166",1)
+
+
